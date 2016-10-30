581f4d7022b6bfaf58802e6d76939e0f4604c463
Jose Machava <jose.s.machava@gmail.com>
2016-10-30 16:25:21 +0200

    save button and count words

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,24 @@
 "use strict"
-window.onload = function(){
 
-    let textarea = "<textarea id='page' class='col-md-12' placeholder='write here'></textarea>"
-    document.body.innerHTML = textarea;
-    var editor = document.getElementById("page");
-    var getValueEditor = editor.value;
+
+window.onload = function () {
+
+    var editor = document.querySelector("#page");
+
+    var save = document.querySelector("#save");
+    save.addEventListener("click", function () {
+        var getValues = editor.value;
+        if(getValues.length == 0){
+            alert("Empty");
+        }else {
+            alert(getValues.trim().length);
+        }
+
+    })
 
 }
+
+
+
+
+
